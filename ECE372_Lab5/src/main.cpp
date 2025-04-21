79dfabd449190a6cf273695041cc02fd5ecbef2a
Jack Tanner <jackhtanner@arizona.edu>
2025-04-21 13:22:21 -0700

    Change

--- ECE372_Lab5/src/main.cpp
+++ ECE372_Lab5/src/main.cpp
@@ -11,8 +11,6 @@ typedef enum
 {
   SMILEY,
   FROWN,
-  DEBOUNCE_PRESS,
-  WAIT
 } StateType;
 
 typedef enum stateName
@@ -37,7 +35,7 @@ int main () {
     initPWMTimer3();
     // Initialize the MAX7219
     initMAX7219();
-
+    InitI2C();
     initSwitchPD2();
 
     sei(); // Enable global interrupts
@@ -102,7 +100,6 @@ volatile bool buttonPressed = false;
 
 ISR(INT0_vect){
   Serial.println("Interrupt triggered");
-<<<<<<< HEAD
   if(buttonState == WAIT_PRESS){
     Serial.println("Pressed");
     buttonState = DEBOUNCE_PRESS;
@@ -111,30 +108,4 @@ ISR(INT0_vect){
     Serial.println("Removed finger");
     buttonState = DEBOUNCE_RELEASE;
   }
-=======
-  delayMs(10); // Small debounce delay
-    
-    if (!(PINB & (1 << PD2))) { // Logic on PD2 is LOW (button pressed)
-        // Flag that the button was pressed!!
-        buttonPressed = true;
-    } 
-    
-    else { // Logic on PD2 is high (button released)
-        // Only toggle face on release, if the button was previously pressed
-        if (buttonPressed) {
-            state = (state == SMILEY) ? FROWN : SMILEY; // Toggle between SMILEY and FROWN
-            buttonPressed = false; // Reset the flag
-            }
-            
-        if (state == SMILEY) {   //Logic for changing display
-          clearDisplay();
-          displaySmileyFace();
-        }
-
-        else {
-          clearDisplay();
-          displayFrownyFace();
-        }
-      }
->>>>>>> e3d234d238cc13deb63f3136f8f5d5a4bb5e96e0
 }
