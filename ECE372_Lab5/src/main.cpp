30769ccc65ab3be5dff72efeb9c810f2a6e6a0ea
Jack Tanner <jackhtanner@arizona.edu>
2025-04-21 12:50:19 -0700

    Update PWM and main

--- ECE372_Lab5/src/main.cpp
+++ ECE372_Lab5/src/main.cpp
@@ -26,6 +26,7 @@ typedef enum stateName
 volatile State buttonState = WAIT_PRESS;
 volatile StateType state = SMILEY;
 volatile bool smiley = true;
+bool chirp = false;
 
 int main () {
   Serial.begin(9600);
@@ -54,12 +55,41 @@ int main () {
 
       case FROWN:
         displayFrownyFace();
+        chirp = true;
         Serial.println("Frowny face displayed");
         break;
 
       default:
-        Serial.println("In defauly LED state.");
+        Serial.println("In default LED state.");
         break;
+    }
+
+    switch(buttonState){
+      case WAIT_PRESS:
+        break;
+      case DEBOUNCE_PRESS:
+        delayMs(1);
+        buttonState = WAIT_RELEASE;
+        break;
+      case WAIT_RELEASE:
+        break;
+      case DEBOUNCE_RELEASE:
+        delayMs(1);
+        chirp = false;
+         buttonState = WAIT_PRESS;
+        break;
+      default:
+        break;
+    }
+
+    if (chirp == true){
+      changeDutyCycle(512);
+      delayMs(500);
+      changeDutyCycle(0);
+      delayMs(500);
+    }
+    else{
+      changeDutyCycle(0);
     }
     
 	}
@@ -70,30 +100,14 @@ int main () {
 volatile bool buttonPressed = false;
 
 
-ISR(PCINT0_vect){
+ISR(INT0_vect){
   Serial.println("Interrupt triggered");
-  delayMs(10); // Small debounce delay
-    
-    if (!(PINB & (1 << PB3))) { // Logic on PB3 is LOW (button pressed)
-        // Flag that the button was pressed!!
-        buttonPressed = true;
-    } 
-    
-    else { // Logic on PB3 is high (button released)
-        // Only toggle face on release, if the button was previously pressed
-        if (buttonPressed) {
-            state = (state == SMILEY) ? FROWN : SMILEY; // Toggle between SMILEY and FROWN
-            buttonPressed = false; // Reset the flag
-            }
-            
-        if (state == SMILEY) {   //Logic for changing display
-          clearDisplay();
-          displaySmileyFace();
-        }
-
-        else {
-          clearDisplay();
-          displayFrownyFace();
-        }
-      }
+  if(buttonState == WAIT_PRESS){
+    Serial.println("Pressed");
+    buttonState = DEBOUNCE_PRESS;
+  }
+  else if(buttonState == WAIT_RELEASE){
+    Serial.println("Removed finger");
+    buttonState = DEBOUNCE_RELEASE;
+  }
 }
